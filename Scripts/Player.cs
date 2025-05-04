881af165ede9d24a3a5e994d94c981f2ee6a4c66
lizixuan126390 <lizixuan126390@pwrd.com>
2025-05-04 17:47:15 +0800

    RPC 消息(发送文字)

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,4 +1,5 @@
 using Fusion;
+using TMPro;
 using UnityEngine;
 
 public class Player : NetworkBehaviour
@@ -84,5 +85,53 @@ public class Player : NetworkBehaviour
         }
         _material.color = Color.Lerp(_material.color, Color.blue, Time.deltaTime);
     }
+
+    private void Update()
+    {
+        if (Object.HasInputAuthority && Input.GetKeyDown(KeyCode.R))
+        {
+            RPC_SendMessage("Bonjour");
+        }
+    }
     
+    
+    //#region rpc
+
+    private TMP_Text _messages;
+
+    /**
+        RpcSources.InputAuthority => 只有對物件具有輸入授權的客戶端才能觸發RPC來發送訊息。
+        RpcTargets.StateAuthority => 發送訊息RPC被發送到主機端（狀態授權）。
+        RpcHostMode.SourceIsHostPlayer => 由於主機端既是伺服器又是客戶端，因此需要指定調用RPC的主機端。
+     */
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority, HostMode = RpcHostMode.SourceIsHostPlayer)]
+    public void RPC_SendMessage(string message, RpcInfo info = default)
+    {
+        RPC_RelayMessage(message, info.Source);
+    }
+
+    /**
+        RpcSources.StateAuthority => 伺服器/主機端正在發送此RPC。
+        RpcTargets.All => 所有客戶端都應接收此RPC。
+        HostMode = RpcHostMode.SourceIsServer => 主機應用程式的伺服器部分正在發送此RPC。
+     */
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All, HostMode = RpcHostMode.SourceIsServer)]
+    public void RPC_RelayMessage(string message, PlayerRef messageSource)
+    {
+        if (_messages == null) _messages = FindObjectOfType<TMP_Text>();
+
+        if (messageSource == Runner.LocalPlayer)
+        {
+            message = $"You said: {message}\n";
+        }
+        else
+        {
+            message = $"Some other player said : {message}\n";
+        }
+
+        _messages.text = message;
+    }
+
+    //#endregion
+
 }
